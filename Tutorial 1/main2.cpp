0c5737f12d1171503dc833375fb4681d992da2f4
7snnn <hassan2.aljadi@live.uwe.ac.uk>
2026-04-21 22:00:29 +0100

    Update main2.cpp

--- Tutorial 1/main2.cpp
+++ Tutorial 1/main2.cpp
@@ -7,9 +7,12 @@ DigitalOut led3(LED3);
 int main()
 {
     while (true) {
+        // toggle all LEDs together
         led1 = !led1;
         led2 = !led2;
         led3 = !led3;
+
+        // wait 0.5 s
         ThisThread::sleep_for(500ms);
     }
 }
